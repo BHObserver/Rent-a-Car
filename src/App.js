178f60ca279d68790116bec41075515898f67643
Ssurafels <ssurafel4@gmail.com>
2024-02-13 15:17:29 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import SignUpForm from './components/sessions/Signup';
+import LoginForm from './components/sessions/Login';
+import Logout from './components/sessions/Logout';
+import Profile from './components/Profile';
 
-function App() {
+
+const App = () => {
+  const user = useSelector((state) => state.auth.user);
+
   return (
-    <div className="App">
-     <p>hello</p>
-    </div>
+      <div>
+        <h1>My App</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            {user ? (
+              <>
+                <li>
+                  <Link to="/profile">Profile</Link>
+                </li>
+                <li>
+                  <Logout />
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/signup">Sign Up</Link>
+                </li>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route exact path="/" element={<h2>Welcome to My App</h2>} />
+          <Route path="/signup" element={<SignUpForm />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
   );
-}
+};
 
 export default App;
