0d1e33b1bf2c25384a87fc8219d5ef6a7e095bbe
BHObserver <BHObserver0@gmail.com>
2024-02-14 21:13:49 +0600

    Update authSlice.js

--- src/redux/reducer/authSlice.js
+++ src/redux/reducer/authSlice.js
@@ -6,36 +6,45 @@ const authSlice = createSlice({
     user: null,
     accessToken: null,
     error: null,
+    successMessage: '',
   },
   reducers: {
     signUpSuccess: (state, action) => {
       state.user = action.payload;
+      state.successMessage = 'Signed up successfully!';
       state.error = null;
     },
     signUpFailure: (state, action) => {
       state.error = action.payload;
+      state.successMessage = '';
     },
     refreshAccessTokenSuccess: (state, action) => {
       state.accessToken = action.payload;
       state.error = null;
+      state.successMessage = '';
     },
     refreshAccessTokenFailure: (state, action) => {
       state.error = action.payload;
+      state.successMessage = '';
     },
     loginSuccess: (state, action) => {
       state.user = action.payload;
       state.error = null;
+      state.successMessage = '';
     },
     loginFailure: (state, action) => {
       state.error = action.payload;
+      state.successMessage = '';
     },
     logoutSuccess: (state) => {
       state.user = null;
       state.accessToken = null;
       state.error = null;
+      state.successMessage = '';
     },
     logoutFailure: (state, action) => {
       state.error = action.payload;
+      state.successMessage = '';
     },
     clearError: (state) => {
       state.error = null;
