a9bbfe5e39127251d84ab14dcb81aff505956844
Ismail Halliru Muhammad <ismailhm888@gmail.com>
2024-02-14 17:05:35 +0100

    Modify entry point

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import ReserveItemPage from './components/ReserveItemPage'; // Import ReserveItemPage component
+import AddItemPage from './components/AddItemPage'; // Import AddItemPage component
+import './App.css'; // Import CSS file for styles
 
-function App() {
+const Reserve = () => <h2>Reserve For Page</h2>;
+const MyReservations = () => <h2>My Reservations Page</h2>;
+const AddItem = () => <h2>Add Item Page</h2>;
+const DeleteItem = () => <h2>Deleted Items Page</h2>;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Sidebar />
+        <main className="contents">
+          <Routes>
+            <Route path="/reserve" element={<Reserve />} />
+            <Route path="/reserve-item" element={<ReserveItemPage />} /> {/* Link to ReserveItemPage */}
+            <Route path="/my-reservations" element={<MyReservations />} />
+            <Route path="/add-item" element={<AddItemPage />} /> {/* Link to AddItemPage */}
+            <Route path="/add-item1" element={<AddItem />} />
+            <Route path="/delete-item" element={<DeleteItem />} />
+            <Route path="/" element={<h2>Home Page</h2>} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
