8f61a24ed848bb4c3538d1b0ad971529741ad2dd
Ssurafels <ssurafel4@gmail.com>
2024-02-15 21:22:38 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
-/* eslint-disable no-unused-vars */
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './reducer/authSlice';
+import carsReducer from './reducer/carReducer';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    cars: carsReducer,
+
   },
 });
 
