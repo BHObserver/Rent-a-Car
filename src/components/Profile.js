62646ce74a6d750097a22eaedac5f54f4ab8027c
BHObserver <BHObserver0@gmail.com>
2024-02-18 04:39:55 +0600

    fixed some styling

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCars } from '../redux/actions/carActions';
-import '../App.css';
+import './Profile.css';
 
 const Profile = () => {
   const dispatch = useDispatch();
--- src/components/navigation/Sidebar.js
+++ src/components/navigation/Sidebar.js
@@ -2,42 +2,42 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Logout from '../sessions/Logout';
+import './Sidebar.css'; // Import CSS file
 
 const Sidebar = () => (
   <div className="sidebar">
-    <NavLink to="/" exact className="logo" activeClassName="active">
+    <NavLink to="/" exact className="logo">
       Your Logo
     </NavLink>
     <nav className="navigation">
-      <ul>
+      <ul className="nav-links">
         <li>
-          <NavLink to="/profile" className="nav-link" activeClassName="active">
-            Home page
+          <NavLink to="/profile" activeClassName="active">
+            Home
           </NavLink>
         </li>
         <li>
-          <NavLink to="/reserve" className="nav-link" activeClassName="active">
+          <NavLink to="/reserve" activeClassName="active">
             Reserve
           </NavLink>
         </li>
         <li>
-          <NavLink to="/my-reservations" className="nav-link" activeClassName="active">
+          <NavLink to="/my-reservations" activeClassName="active">
             My Reservations
           </NavLink>
         </li>
-
         <li>
-          <NavLink to="/add-item" className="nav-link" activeClassName="active">
+          <NavLink to="/add-item" activeClassName="active">
             Add Car
           </NavLink>
         </li>
         <li>
-          <NavLink to="/delete-item" className="nav-link" activeClassName="active">
+          <NavLink to="/delete-item" activeClassName="active">
             Delete Car
           </NavLink>
         </li>
         <li>
-          <NavLink to="/login" className="nav-link" activeClassName="active">
+          <NavLink to="/login">
             <Logout />
           </NavLink>
         </li>
