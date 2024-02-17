f9ad1fca32a3627bd23cd278a5242cd95861d4cd
Ssurafels <ssurafel4@gmail.com>
2024-02-17 12:00:30 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 import './index.css';
 
 const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 
-ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
