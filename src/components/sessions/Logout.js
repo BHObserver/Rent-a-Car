b85a9e9723b751037ce0cfae12494969042131f3
BHObserver <BHObserver0@gmail.com>
2024-02-22 03:45:13 +0600

    Update Logout.js

--- src/components/sessions/Logout.js
+++ src/components/sessions/Logout.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate hook for navigation
 import { logout } from '../../redux/session/actions/authActions';
 
 const Logout = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate(); // Get navigate function from useNavigate hook
 
-  const handleLogout = () => {
-    dispatch(logout());
+  const handleLogout = async () => {
+    try {
+      dispatch(logout()); // Wait for logout action to complete
+      navigate('/login'); // Navigate to login page after logout
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
   };
 
   return (
     <div>
-      <button type="submit" onClick={handleLogout}>Logout</button>
+      <button type="button" onClick={handleLogout}>Logout</button>
     </div>
   );
 };
